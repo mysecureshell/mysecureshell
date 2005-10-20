5acf653c70aa83d8f97fe34d76fee1ba13c74dee
teka <>
2005-10-20 20:10:56 +0000

    Fixe la version a 0.7

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ static void	parse_args(int ac, char **av)
     else if (!strcmp(av[i], "--help"))
       {
       help:
-	printf("Build:\n\t%s is version %s build on " __DATE__ "\n\n", av[0], "0.7b4");
+	printf("Build:\n\t%s is version %s build on " __DATE__ "\n\n", av[0], "0.7");
 	printf("Usage:\n\t%s [verbose] [options]\n\nOptions:\n", av[0]);
 	printf("\t--configtest : test the config file and show errors\n");
 	printf("\t--help       : show this screen\n");
@@ -236,12 +236,12 @@ int	main(int ac, char **av, char **env)
 	}
       delete_hash();
 
-#ifdef DODEBUG
+      /*#ifdef DODEBUG
       env[0] = "EF_DISABLE_BANNER=1";
       env[1] = "EF_FREE_WIPES=1";
       env[2] = "EF_PROTECT_FREE=1";
       env[3] = "EF_ALLOW_MALLOC_0=1";
-#endif
+      #endif*/
 
       execve(exe, args, env);
       exit (1);
