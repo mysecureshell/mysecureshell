e3368d10267ed41ecba58985dadfb1a17624b5b3
teka <>
2005-11-09 17:40:26 +0000

    Mise en place des autotools

--- conf.c
+++ conf.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <string.h>
 #include "conf.h"
-#include "defines.h"
+#include "config.h"
 #include "ip.h"
 #include "parsing.h"
 #include "string.h"
