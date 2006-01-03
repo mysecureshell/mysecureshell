ad0703b86d454024412c3554d4bb1fd9ac7bd97b
teka <>
2006-01-03 18:20:37 +0000

    - Ajout du support des ACL - Ajout d'un print de debug en prevision du procotol v5

--- SftpServer/Encode.c
+++ SftpServer/Encode.c
@@ -18,13 +18,55 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "../config.h"
+#include <sys/types.h>
+#include <sys/ioctl.h>
 #include <grp.h>
 #include <pwd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#ifdef HAVE_LINUX_EXT2_FS_H
+
+#define EXT2_SECRM_FL                   0x00000001 /* Secure deletion */
+#define EXT2_UNRM_FL                    0x00000002 /* Undelete */
+#define EXT2_COMPR_FL                   0x00000004 /* Compress file */
+#define EXT2_SYNC_FL                    0x00000008 /* Synchronous updates */
+#define EXT2_IMMUTABLE_FL               0x00000010 /* Immutable file */
+#define EXT2_APPEND_FL                  0x00000020 /* writes to file may only append */
+#define EXT2_NODUMP_FL                  0x00000040 /* do not dump file */
+#define EXT2_NOATIME_FL                 0x00000080 /* do not update atime */
+/* Reserved for compression usage... */
+#define EXT2_DIRTY_FL                   0x00000100
+#define EXT2_COMPRBLK_FL                0x00000200 /* One or more compressed clusters */
+#define EXT2_NOCOMP_FL                  0x00000400 /* Don't compress */
+#define EXT2_ECOMPR_FL                  0x00000800 /* Compression error */
+/* End compression flags --- maybe not all used */
+#define EXT2_BTREE_FL                   0x00001000 /* btree format dir */
+#define EXT2_INDEX_FL                   0x00001000 /* hash-indexed directory */
+#define EXT2_IMAGIC_FL                  0x00002000 /* AFS directory */
+#define EXT2_JOURNAL_DATA_FL            0x00004000 /* Reserved for ext3 */
+#define EXT2_NOTAIL_FL                  0x00008000 /* file tail should not be merged */
+#define EXT2_DIRSYNC_FL                 0x00010000 /* dirsync behaviour (directories only) */
+#define EXT2_TOPDIR_FL                  0x00020000 /* Top of directory hierarchies*/
+#define EXT2_RESERVED_FL                0x80000000 /* reserved for ext2 lib */
+
+#define EXT2_FL_USER_VISIBLE            0x0003DFFF /* User visible flags */
+#define EXT2_FL_USER_MODIFIABLE         0x000380FF /* User modifiable flags */
+
+/*
+ * ioctl commands
+ */
+#define EXT2_IOC_GETFLAGS               _IOR('f', 1, long)
+#define EXT2_IOC_SETFLAGS               _IOW('f', 2, long)
+#define EXT2_IOC_GETVERSION             _IOR('v', 1, long)
+#define EXT2_IOC_SETVERSION             _IOW('v', 2, long)
+
+#endif
+#include <unistd.h>
 #include "Encode.h"
 #include "GetUsersInfos.h"
+#include "Log.h"
 
 tAttributes		*GetAttributes(tBuffer *bIn)
 {
@@ -32,6 +74,7 @@ tAttributes		*GetAttributes(tBuffer *bIn)
 
   memset(&a, 0, sizeof(a));
   a.flags = BufferGetInt32(bIn);
+  DEBUG((MYLOG_DEBUG, "FLAGS[%x][%i]", a.flags, a.flags));
   if (cVersion >= 4)
     a.type = BufferGetInt8(bIn);
   if (a.flags & SSH2_FILEXFER_ATTR_SIZE)
@@ -94,7 +137,7 @@ tAttributes		*GetAttributes(tBuffer *bIn)
   return (&a);
 }
 
-void	StatToAttributes(struct stat *st, tAttributes *a)
+void	StatToAttributes(struct stat *st, tAttributes *a, char *fileName)
 {
   memset(a, 0, sizeof(*a));
   a->flags = SSH2_FILEXFER_ATTR_SIZE;
@@ -117,11 +160,53 @@ void	StatToAttributes(struct stat *st, tAttributes *a)
 	a->type = SSH4_FILEXFER_TYPE_SYMLINK;
       else
 	a->type = SSH4_FILEXFER_TYPE_SPECIAL;
+      if (cVersion >= 5)
+	{
+	  if ((st->st_mode & S_IFMT) == S_IFSOCK)
+	    a->type = SSH5_FILEXFER_TYPE_SOCKET;
+	  else if ((st->st_mode & S_IFMT) == S_IFCHR)
+	    a->type = SSH5_FILEXFER_TYPE_CHAR_DEVICE;
+	  else if ((st->st_mode & S_IFMT) == S_IFBLK)
+	    a->type = SSH5_FILEXFER_TYPE_BLOCK_DEVICE;
+	  else if ((st->st_mode & S_IFMT) == S_IFIFO)
+	    a->type = SSH5_FILEXFER_TYPE_FIFO;
+	}
       a->flags |= SSH4_FILEXFER_ATTR_OWNERGROUP | SSH4_FILEXFER_ATTR_ACCESSTIME
 	| SSH4_FILEXFER_ATTR_CREATETIME | SSH4_FILEXFER_ATTR_MODIFYTIME;
     }
   else
     a->flags |= SSH2_FILEXFER_ATTR_UIDGID;
+  if (cVersion >= 5 && fileName)
+    {
+      int	pos = strlen(fileName) - 1;
+      int	fd;
+
+      a->attrib = 0;
+      a->flags |= SSH5_FILEXFER_ATTR_BITS;
+      while (pos >= 1 && fileName[pos - 1] != '/')
+	pos--;
+      if (pos >= 0 && fileName[pos] == '.')
+      a->attrib |= SSH5_FILEXFER_ATTR_FLAGS_HIDDEN;
+#ifdef HAVE_LINUX_EXT2_FS_H
+      if ((fd = open(fileName, O_RDONLY)) >= 0)
+	{
+	  int	flags;
+
+	  if (ioctl(fd, EXT2_IOC_GETFLAGS, flags) != -1)
+	    {
+	      if (flags & EXT2_COMPR_FL)
+		a->attrib |= SSH5_FILEXFER_ATTR_FLAGS_COMPRESSED;
+	      if (flags & EXT2_APPEND_FL)
+		a->attrib |= SSH5_FILEXFER_ATTR_FLAGS_APPEND_ONLY;
+	      if (flags & EXT2_IMMUTABLE_FL)
+		a->attrib |= SSH5_FILEXFER_ATTR_FLAGS_IMMUTABLE;
+	      if (flags & EXT2_SYNC_FL)
+		a->attrib |= SSH5_FILEXFER_ATTR_FLAGS_SYNC;
+	    }
+	  close(fd);
+	}
+#endif
+    }
 }
 
 void	EncodeAttributes(tBuffer *b, tAttributes *a)
@@ -185,6 +270,8 @@ void	EncodeAttributes(tBuffer *b, tAttributes *a)
       if (a->flags & SSH4_FILEXFER_ATTR_SUBSECOND_TIMES)
 	BufferPutInt32(b, 0);
     }
+  if (a->flags & SSH5_FILEXFER_ATTR_BITS)
+    BufferPutInt32(b, a->attrib);
   if (a->flags & SSH2_FILEXFER_ATTR_ACL)
     BufferPutString(b, ""); //unsupported feature
   if (a->flags & SSH2_FILEXFER_ATTR_EXTENDED)
