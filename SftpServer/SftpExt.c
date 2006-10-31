71c38d968b4c6200e6ac19f9e2bb854267360581
teka <>
2006-10-31 22:37:04 +0000

    Amelioration du support de l'extension sftp check-file

--- SftpServer/SftpExt.c
+++ SftpServer/SftpExt.c
@@ -22,12 +22,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 #include "Buffer.h"
 #include "Encoding.h"
 #include "Handle.h"
+#include "Log.h"
 #include "Send.h"
 #include "SftpExt.h"
 #include "SftpServer.h"
@@ -42,7 +41,7 @@ void	DoExtDiskSpace(tBuffer *bIn, tBuffer *bOut, u_int32_t id)
   int		status;
 
   path = convertFromUtf8(BufferGetString(bIn), 1);
-  if ((status = CheckRules(path, RULES_DIRECTORY, 0, O_RDONLY)) == SSH2_FX_OK)
+  if ((status = CheckRules(path, RULES_DIRECTORY, NULL, O_RDONLY)) == SSH2_FX_OK)
     {
       if (!STATFS(path, &stfs))
 	{
@@ -65,13 +64,14 @@ void	DoExtDiskSpace(tBuffer *bIn, tBuffer *bOut, u_int32_t id)
     SendStatus(bOut, id, status);
   free(path);
 }
-#endif
+#endif //MSSEXT_DISKUSAGE
 
 #ifdef MSSEXT_FILE_HASHING
 #include <openssl/evp.h>
 
 static void DoExtFileHashing_FD(tBuffer *bIn, tBuffer *bOut, u_int32_t id, int fd)
 {
+  const EVP_MD	*md;
   u_int64_t	offset, length;
   u_int32_t	blockSize;
   char		*algo;
@@ -81,52 +81,84 @@ static void DoExtFileHashing_FD(tBuffer *bIn, tBuffer *bOut, u_int32_t id, int f
   length = BufferGetInt64(bIn);
   blockSize = BufferGetInt32(bIn);
   if (lseek(fd, offset, SEEK_SET) == -1)
-    SendStatus(bOut, id, errnoToPortable(errno));
-  else
     {
-      const EVP_MD	*md;
-
-      if (!blockSize)
-	blockSize = length;
-      if ((md = EVP_get_digestbyname(algo)))
+      SendStatus(bOut, id, errnoToPortable(errno));
+      DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Error lseek1"));
+      goto endOfFileHashing;
+    }
+  if (length == 0)//read the file to the end
+    {
+      u_int64_t	endOfFile;
+      
+      if ((endOfFile = lseek(fd, 0, SEEK_END)) == -1)
 	{
-	  unsigned char	md_value[EVP_MAX_MD_SIZE];
-	  EVP_MD_CTX	mdctx;
-	  u_int32_t	md_len;
-	  tBuffer	*b;
-	  char		*data;
+	  SendStatus(bOut, id, errnoToPortable(errno));
+	  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Error lseek2"));
+	  goto endOfFileHashing;
+	}
+      length = endOfFile - offset;
+      if (lseek(fd, offset, SEEK_SET) == -1)
+	{
+	  SendStatus(bOut, id, errnoToPortable(errno));
+	  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Error lseek3"));
+	  goto endOfFileHashing;
+	}
+    }
+  if (blockSize == 0)//read length in one time
+    blockSize = length;
+  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Algo:%s Fd:%i Offset:%llu Length:%llu BlockSize:%i",
+	 algo, fd, offset, length, blockSize));
+  if ((md = EVP_get_digestbyname(algo)))
+    {
+      unsigned char	md_value[EVP_MAX_MD_SIZE];
+      EVP_MD_CTX	mdctx;
+      u_int32_t		md_len;
+      tBuffer		*b;
+      char		*data;
+      
+      EVP_MD_CTX_init(&mdctx);
+      EVP_DigestInit_ex(&mdctx, md, NULL);
+      data = malloc(blockSize);
+      if (data != NULL)
+	{
+	  u_int64_t	off = 0;
+	  u_int32_t	r;
 	  
-	  EVP_MD_CTX_init(&mdctx);
-	  EVP_DigestInit_ex(&mdctx, md, NULL);
-	  data = malloc(blockSize);
-	  if (data != NULL)
+	  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Read(%i)", blockSize));
+	  while ((r = read(fd, data, blockSize)) > 0)
 	    {
-	      u_int64_t	off = 0;
-	      u_int32_t r;
-
-	      while ((r = read(fd, data, blockSize)) > 0)
-		{
-		  EVP_DigestUpdate(&mdctx, data, r);
-		  off += (u_int64_t )r;
-		  if ((off + (u_int64_t )blockSize) > length)
-		    blockSize = (u_int32_t )(length - off);
-		  if (off == length)
-		    break;
-		}
-	      free(data);
+	      DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Compute block (%i/%i)", r, blockSize));
+	      EVP_DigestUpdate(&mdctx, data, r);
+	      off += (u_int64_t )r;
+	      if ((off + (u_int64_t )blockSize) > length)
+		blockSize = (u_int32_t )(length - off);
+	      if (off == length)
+		break;
 	    }
+	  free(data);
 	  EVP_DigestFinal_ex(&mdctx, md_value, &md_len);
-	  EVP_MD_CTX_cleanup(&mdctx);
-          b = BufferNew();
-          BufferPutInt8(b, SSH2_FXP_EXTENDED_REPLY);
-          BufferPutInt32(b, id);
+	  b = BufferNew();
+	  BufferPutInt8(b, SSH2_FXP_EXTENDED_REPLY);
+	  BufferPutInt32(b, id);
 	  BufferPutString(b, algo);
-	  BufferPutData(b, md_value, md_len);
-          BufferPutPacket(bOut, b);
+	  BufferPutRawData(b, md_value, md_len);
+	  BufferPutPacket(bOut, b);
+	  DEBUG((MYLOG_DEBUG, "[%X%X%X]", md_value[0], md_value[1], md_value[2]));
 	}
       else
-	SendStatus(bOut, id, SSH2_FX_OP_UNSUPPORTED);
+	{
+	  SendStatus(bOut, id, errnoToPortable(errno));
+	  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]Error malloc"));
+	}
+      EVP_MD_CTX_cleanup(&mdctx);
     }
+  else
+    {
+      DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]No algorithm: %s", algo));
+      SendStatus(bOut, id, SSH2_FX_OP_UNSUPPORTED);
+    }
+ endOfFileHashing:
+  DEBUG((MYLOG_DEBUG, "[DoExtFileHashing_FD]End"));
   free(algo);
 }
 
@@ -134,7 +166,7 @@ void    DoExtFileHashing_Handle(tBuffer *bIn, tBuffer *bOut, u_int32_t id)
 {
   int	fd, fileIsText;
 
-  if ((fd = HandleGetFd(BufferGetStringAsInt(bIn), &fileIsText)) >= 0)
+  if ((fd = HandleGetFd(BufferGetHandle(bIn), &fileIsText)) >= 0)
     DoExtFileHashing_FD(bIn, bOut, id, fd);
   else
     SendStatus(bOut, id, SSH4_FX_INVALID_HANDLE);
@@ -143,15 +175,20 @@ void    DoExtFileHashing_Handle(tBuffer *bIn, tBuffer *bOut, u_int32_t id)
 void    DoExtFileHashing_Name(tBuffer *bIn, tBuffer *bOut, u_int32_t id)
 {
   char	*file = BufferGetString(bIn);
-  int	fd;
+  int	status, fd;
 
-  if ((fd = open(file, O_RDONLY)) != -1)
+  if ((status = CheckRules(file, RULES_FILE, NULL, O_RDONLY)) == SSH2_FX_OK)
     {
-      DoExtFileHashing_FD(bIn, bOut, id, fd);
-      close(fd);
+      if ((fd = open(file, O_RDONLY)) != -1)
+	{
+	  DoExtFileHashing_FD(bIn, bOut, id, fd);
+	  close(fd);
+	}
+      else
+	SendStatus(bOut, id, errnoToPortable(errno));
     }
   else
-    SendStatus(bOut, id, errnoToPortable(errno));
+    SendStatus(bOut, id, status);
   free(file);
 }
-#endif
+#endif //MSSEXT_FILE_HASHING
