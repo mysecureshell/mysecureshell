80c4ddd9bfe010ecedc3f39d499e7a78f2039493
teka <>
2007-03-19 18:44:58 +0000

    Ajout d'includes pour OSX

--- SftpServer/SftpExt.c
+++ SftpServer/SftpExt.c
@@ -22,6 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "Buffer.h"
 #include "Encoding.h"
