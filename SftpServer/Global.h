7981eb85f50574c36fd2b703e22e6aae5905f468
teka <>
2007-05-08 16:22:23 +0000

    Mise au propre du fichier (reindentation)

--- SftpServer/Global.h
+++ SftpServer/Global.h
@@ -22,8 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "Buffer.h"
 
-extern tBuffer *bIn;
-extern tBuffer *bOut;
-extern tGlobal *gl_var;
+extern tBuffer	*bIn;
+extern tBuffer	*bOut;
+extern tGlobal	*gl_var;
 
 #endif
