eb8d94f0ab2e35124a32063018d48e62785da534
teka <>
2007-07-16 10:46:19 +0000

    Optimisation de la fonction 'InitAccess' pour les serveurs qui ont beaucoup de groupes

--- SftpServer/Access.c
+++ SftpServer/Access.c
@@ -17,98 +17,46 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#include "../config.h"
+#include <errno.h>
 #include <grp.h>
 #include <pwd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 static gid_t	*_in_group = 0;
 
-#ifndef HAVE_GETGROUPLIST
-static int getgrouplist(const char *uname, gid_t agroup, register gid_t *groups, int *grpcnt)
+void	InitAccess()
 {
-  register struct group *grp;
-  register int i, ngroups;
-  int ret, maxgroups;
-  int bail;
+  gid_t	*groups;
+  int	nb_groups = 8, ret;
 
-  ret = 0;
-  ngroups = 0;
-  maxgroups = *grpcnt;
-  
-  /*
-   * install primary group
-   */
-  if (ngroups >= maxgroups) {
-    *grpcnt = ngroups;
-    return (-1);
-  }
-  groups[ngroups++] = agroup;
-  
-  /*
-   * Scan the group file to find additional groups.
-   */
-  (void )setgrent();
-  while ((grp = getgrent())) {
-    if (grp->gr_gid == agroup)
-      continue;
-    for (bail = 0, i = 0; bail == 0 && i < ngroups; i++)
-      if (groups[i] == grp->gr_gid)
-	bail = 1;
-    if (bail)
-      continue;
-    for (i = 0; grp->gr_mem[i]; i++) {
-      if (strcmp(grp->gr_mem[i], uname) == 0) {
-	if (ngroups >= maxgroups) {
-	  ret = -1;
-	  goto out;
-	}
-	groups[ngroups++] = grp->gr_gid;
-	break;
-      }
-    }
-  }
- out:
-  endgrent();
-  *grpcnt = ngroups;
-  return (ret);
-}
-#endif
-
-static void	InitAccess()
-{
-  struct passwd	*info;
-  struct group	*group;
-  gid_t		*groups;
-  int		nb_groups = 42;
-  int		i;
-
-  if ((info = getpwuid(getuid())) != NULL)
+  groups = malloc(nb_groups * sizeof(*groups));
+  if ((ret = getgroups(nb_groups, groups)) == -1)
     {
-      groups = malloc(nb_groups * sizeof(*groups));
-      if (getgrouplist(info->pw_name, info->pw_gid, groups, &nb_groups) == -1)
-	{
-	  groups = realloc(groups, nb_groups * sizeof(*groups));
-	  (void )getgrouplist(info->pw_name, info->pw_gid, groups, &nb_groups);
-	}
-      if (nb_groups > 0)
-        {
-          _in_group = malloc((nb_groups + 1) * sizeof(*_in_group));
-          for (i = 0; i < nb_groups; i++)
-	    if ((group = getgrgid(groups[i])) != NULL)
-	      _in_group[i] = group->gr_gid;
-          _in_group[i] = 0;
-        }
-      free(groups);
+      if (errno == EINVAL)
+	nb_groups = 63;
+      else
+	nb_groups = 0;
     }
+  else
+    nb_groups = ret;
+  _in_group = malloc((nb_groups + 2) * sizeof(*_in_group));
+  if (nb_groups > 0)
+    if (getgroups(nb_groups, _in_group) == -1)
+      nb_groups = 0;
+  _in_group[nb_groups] = getgid();
+  _in_group[nb_groups + 1] = -1;
+  free(groups);
 }
 
-static int	UserIsInThisGroup(gid_t grp)
+int	UserIsInThisGroup(gid_t grp)
 {
-  int		i;
+  int	i;
 
   if (_in_group != NULL)
-    for (i = 0; _in_group[i] != 0; i++)
+    for (i = 0; _in_group[i] != -1; i++)
       if (_in_group[i] == grp)
 	return (1);
   return (0);
