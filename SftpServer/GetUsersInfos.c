4f2b4494d9b8c1f955621c4bc05ac305b6ed6f63
teka <>
2007-08-25 13:15:47 +0000

    Optimisation dans le cas ou l'utilisateur n'est pas chrooter : on ne charge plus en memoire tous les utilisateurs / groupes, on charge seulement sur demande (et si l'information n'est pas deja dans le tableau)

--- SftpServer/GetUsersInfos.c
+++ SftpServer/GetUsersInfos.c
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "../config.h"
+#include <sys/types.h>
 #include <grp.h>
 #include <pwd.h>
 #include <stdlib.h>
@@ -26,46 +27,75 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "GetUsersInfos.h"
 #include "Log.h"
 
+#define	TB_ALLOC_SIZE	42
+
 static t_info	*_users = NULL;
 static t_info	*_groups = NULL;
+static int	_usersSize = 0;
+static int	_groupsSize = 0;
+
+static t_info	*add_element_in_table(t_info **tb, int *tbSize, char *name, int id)
+{
+  DEBUG((MYLOG_DEBUG, "add_element_in_table(%p, %i, %s, %i)", *tb, *tbSize, name, id));
+  if ((*tbSize % TB_ALLOC_SIZE) == 0)
+    {
+      *tb = realloc(*tb, *tbSize + TB_ALLOC_SIZE);
+      DEBUG((MYLOG_DEBUG, "Increaze size to: %i", *tbSize + TB_ALLOC_SIZE));
+    }
+  (*tb)[*tbSize].name = name;
+  (*tb)[*tbSize].id = id;
+  return &((*tb)[(*tbSize)++]);
+}
 
 void		init_usersinfos()
 {
-  struct passwd	*pw;
-  struct group	*grp;
-  int		size;
-
-  (void )setpwent();
-  for (size = 0; (pw = getpwent()) != NULL; size++)
+  if (HAS_BIT(gl_var->who->status, SFTPWHO_VIRTUAL_CHROOT))
     {
-      _users = realloc(_users, (size + 2) * sizeof(*_users));
-      _users[size].name = strdup(pw->pw_name);
-      _users[size].id = pw->pw_uid;
+      struct passwd	*pw;
+      struct group	*grp;
+      int		size;
+     
+      DEBUG((MYLOG_DEBUG, "Load all users..."));
+      (void )setpwent();
+      for (size = 0; (pw = getpwent()) != NULL; size++)
+	{
+	  _users = realloc(_users, (size + 2) * sizeof(*_users));
+	  _users[size].name = strdup(pw->pw_name);
+	  _users[size].id = pw->pw_uid;
+	}
+      endpwent();
+      _usersSize = size;
+      DEBUG((MYLOG_DEBUG, "Load all groups..."));
+      (void )setgrent();
+      for (size = 0; (grp = getgrent()) != NULL; size++)
+	{
+	  _groups = realloc(_groups, (size + 2) * sizeof(*_groups));
+	  _groups[size].name = strdup(grp->gr_name);
+	  _groups[size].id = grp->gr_gid;
+	}
+      endgrent();
+      _groupsSize = size;
     }
-  endpwent();
-
-  (void )setgrent();
-  for (size = 0; (grp = getgrent()) != NULL; size++)
-    {
-      _groups = realloc(_groups, (size + 2) * sizeof(*_groups));
-      _groups[size].name = strdup(grp->gr_name);
-      _groups[size].id = grp->gr_gid;
-    }
-  endgrent();
 }
 
 t_info	*mygetpwnam(const char *login)
 {
   int	i;
 
-  if (_users != NULL && login != NULL)
+  if (login == NULL)
+    return (NULL);
+  for (i = 0; i < _usersSize; i++)
+    if (strcmp(_users[i].name, login) == 0)
+      return (&_users[i]);
+  if (!HAS_BIT(gl_var->who->status, SFTPWHO_VIRTUAL_CHROOT))
     {
-      for (i = 0; _users[i].name != NULL; i++)
-        if (strcmp(_users[i].name, login) == 0)
-	  return (&_users[i]);
-      mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve user name %i",
-                 gl_var->who->user, gl_var->who->ip, login);
+      struct passwd	*pwd;
+
+      if ((pwd = getpwnam(login)) != NULL)
+	return (add_element_in_table(&_users, &_usersSize, strdup(pwd->pw_name), pwd->pw_uid));
     }
+  mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve user name %i",
+	       gl_var->who->user, gl_var->who->ip, login);
   return (NULL);
 }
 
@@ -74,14 +104,18 @@ t_info	*mygetpwuid(uid_t uid)
 {
   int	i;
 
-  if (_users != NULL)
+  for (i = 0; i < _usersSize; i++)
+    if (_users[i].id == uid)
+      return (&_users[i]);
+  if (!HAS_BIT(gl_var->who->status, SFTPWHO_VIRTUAL_CHROOT))
     {
-      for (i = 0; _users[i].name != NULL; i++)
-	if (_users[i].id == uid)
-	  return (&_users[i]);
-      mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve user id %i",
-		 gl_var->who->user, gl_var->who->ip, uid);
+      struct passwd	*pwd;
+
+      if ((pwd = getpwuid(uid)) != NULL)
+	return (add_element_in_table(&_users, &_usersSize, strdup(pwd->pw_name), pwd->pw_uid));
     }
+  mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve user id %i",
+	       gl_var->who->user, gl_var->who->ip, uid);
   return (NULL);
 }
 
@@ -89,14 +123,20 @@ t_info	*mygetgrnam(const char *group)
 {
   int	i;
 
-  if (_groups != NULL && group != NULL)
+  if (group == NULL)
+    return (NULL);
+  for (i = 0; i < _groupsSize; i++)
+    if (strcmp(_groups[i].name, group) == 0)
+      return (&_groups[i]);
+  if (!HAS_BIT(gl_var->who->status, SFTPWHO_VIRTUAL_CHROOT))
     {
-      for (i = 0; _groups[i].name != NULL; i++)
-	if (strcmp(_groups[i].name, group) == 0)
-	  return (&_groups[i]);
-      mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve group name %i",
-		 gl_var->who->user, gl_var->who->ip, group);
+      struct group	*grp;
+
+      if ((grp = getgrnam(group)) != NULL)
+	return (add_element_in_table(&_groups, &_groupsSize, strdup(grp->gr_name), grp->gr_gid));
     }
+  mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve group name %i",
+	       gl_var->who->user, gl_var->who->ip, group);
   return (NULL);
 }
 
@@ -104,13 +144,17 @@ t_info	*mygetgrgid(gid_t gid)
 {
   int	i;
 
-  if (_groups != NULL)
+  for (i = 0; i < _groupsSize; i++)
+    if (_groups[i].id == gid)
+      return (&_groups[i]);
+  if (!HAS_BIT(gl_var->who->status, SFTPWHO_VIRTUAL_CHROOT))
     {
-      for (i = 0; _groups[i].name != NULL; i++)
-	if (_groups[i].id == gid)
-	  return (&_groups[i]);
-      mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve group id %i",
-		 gl_var->who->user, gl_var->who->ip, gid);
+      struct group	*grp;
+
+      if ((grp = getgrgid(gid)) != NULL)
+	return (add_element_in_table(&_groups, &_groupsSize, strdup(grp->gr_name), grp->gr_gid));
     }
+  mylog_printf(MYLOG_WARNING, "[%s][%s]Couldn't resolve group id %i",
+	       gl_var->who->user, gl_var->who->ip, gid);
   return (NULL);
 }
