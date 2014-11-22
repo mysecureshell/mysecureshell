30b6c022720908644c03607e631c352d21e0eee3
Teka101 <Teka101@github.com>
2014-11-22 18:50:19 +0100

    Fix cygwin method.

--- SftpServer/FileSystemAcl.c
+++ SftpServer/FileSystemAcl.c
@@ -126,7 +126,7 @@ int FSEnumAcl(const char *file, int resolvePath, void (*callback)(void *data, in
 
 #else //ifdef HAVE_CYGWIN
 
-int FSEnumAcl(const char *file, int resolvePath, void (*callback)(void *data, int type, u_int32_t id, u_int32_t mode), void *data, int *nbEntries)
+int FSEnumAcl(const char *file, int resolvePath, void (*callback)(void *data, int type, u_int32_t id, u_int32_t mode), void *data, u_int32_t *nbEntries)
 {
 	aclent_t acls[MAX_ACL_ENTRIES];
 	tFSPath	*path;
