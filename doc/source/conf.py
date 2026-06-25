fb49f1e5a042419e182250488fa7c6200d56229e
Teka101 <Teka101@users.noreply.github.com>
2026-06-25 12:31:38 +0200

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -271,7 +271,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
 
 # -- ReadTheDoc requirements and local template generation---------------------
 
